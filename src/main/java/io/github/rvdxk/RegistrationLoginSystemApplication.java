52bdd9c7cbafec642ad34e4cdfcdf51c29e6d147
rvdxk <radek.szczot@gmail.com>
2024-04-23 20:03:18 +0200

    init project

---
+++ src/main/java/io/github/rvdxk/RegistrationLoginSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.rvdxk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSystemApplication.class, args);
+	}
+
+}
